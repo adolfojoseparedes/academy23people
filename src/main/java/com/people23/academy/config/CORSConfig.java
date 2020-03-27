043ab85e10ccf6c692c3a391b8ed9811bcc0a48e
adolfojoseparedes <adolfojoseparedes@gmail.com>
2020-03-27 08:31:58 -0300

    Changes for GCP. Added comment for the new class. v2

--- src/main/java/com/people23/academy/config/CORSConfig.java
+++ src/main/java/com/people23/academy/config/CORSConfig.java
@@ -1,14 +1,23 @@
 package com.people23.academy.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Class for configuration Web MVC
+ * 
+ * @author 23 People Company
+ *
+ */
 @Configuration
 public class CORSConfig {
 
+	/**
+    * Method type bean
+    * @return WebMvcConfigurer value
+    */
     @Bean
     public WebMvcConfigurer CORSConfigurer(){
         return new WebMvcConfigurer() {
@@ -20,5 +29,4 @@ public class CORSConfig {
             }
         };
     }
-
 }
